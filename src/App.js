d21bea23352a6986c586d81c315df12de8e15125
Dara Heng <daraheng.ncdd@gmail.com>
2017-07-30 11:12:06 +0700

    Add Routes to main App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 //import * as BooksAPI from './BooksAPI'
 import BookSearch from './BookSearch'
 import './App.css'
 
 class BooksApp extends Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true
   }
 
   render() {
-
-
     return (
       <div className="app">
-        <BookSearch />
-        <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-        </div>
-       </div >
+        <Route exact path="/" render={() => (
+          <div>
+            <h1>Book List</h1>
+            <div className="open-search">
+              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            </div>
+          </div>
+        )} />
+
+        <Route exact path="/search" component={BookSearch} />
+
+      </div >
     )
   }
 }
